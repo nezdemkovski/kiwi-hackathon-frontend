bb0460be834ec3702c063e96a72cbced96a05096
Alex Alexeev <aliakseyeu@cngroup.dk>
2017-06-23 23:29:46 +0200

    first change

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentWillMount() {
+    fetch(
+      'https://api.sygictravelapi.com/0.2/en/places/list?tags=hotel&levels=poi&parents=country:5',
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'x-api-key': 'nSCQiwW9R88zlr0P7J2VocUXBnKejmO26m9eIUl8',
+        },
+      },
+    ).then(resp => {
+      console.log(response);
+    });
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
