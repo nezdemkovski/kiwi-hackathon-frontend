f702156499119c69c1ba2547b3ed2c64864e7e85
Jan Toman <toman.jan@gmail.com>
2017-06-24 12:09:23 +0200

    newsports

--- src/App.js
+++ src/App.js
@@ -44,6 +44,16 @@ class App extends Component {
         tagQuery: 'sailing',
         nameC: 'sailing',
       },
+      {
+        name: 'Kayaking',
+        tagQuery: 'kayaking',
+        nameC: 'kayaking',
+      },
+      {
+        name: 'Dolphin swimming',
+        tagQuery: 'dolphin',
+        nameC: 'dolphin',
+      }
     ];
   }
   getData = () => {
