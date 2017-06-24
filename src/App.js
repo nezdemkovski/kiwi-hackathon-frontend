7a535f351f1c127b048b1047573511e4f2c5c647
Alex Alexeev <aliakseyeu@cngroup.dk>
2017-06-24 16:06:12 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import CitiesCount from './CitiesCount';
 import Search from './Search';
+import Sidebar from './Sidebar';
 import './static/css/index.css';
 
 class App extends Component {
@@ -24,7 +24,7 @@ class App extends Component {
       },
       {
         name: 'Bars / pubs',
-        tagQuery: 'bar',
+        tagQuery: 'Hotel',
         nameC: 'bar',
       },
       {
@@ -44,7 +44,7 @@ class App extends Component {
       },
       {
         name: 'Spa / massages',
-        tagQuery: 'spa|massages',
+        tagQuery: 'spa',
         nameC: 'massages',
       },
       {
@@ -148,59 +148,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <div className="column--left">
-            <div className="logo" />
-
-            <div className="title">Where do you wanna go?</div>
-            <div className="location">
-              <div className="place">
-                <i className="fa fa-globe" aria-hidden="true" /> Literally
-                anywhere.
-              </div>
-              <div className="button--link">choose some</div>
-            </div>
-
-            <div className="title">What will you enjoy on your trip?</div>
-
-            {this.state.chosenTiles.map(tile =>
-              <div
-                className={`interest ${tile.nameC}`}
-                key={tile.tagQuery + tile.name}
-              >
-                <div className="name">
-                  <div className="interest-icon" />
-                  {tile.name}
-                </div>
-                <div className="icon" onClick={() => this.removeTile(tile)}>
-                  <i className="fa fa-times" aria-hidden="true" />
-                </div>
-              </div>,
-            )}
-
-            {this.state.chosenTiles.length == 0
-              ? <div className="empty-state">
-                  Select some activities.{' '}
-                  <i className="fa fa-arrow-right" aria-hidden="true" />{' '}
-                  <i className="fa fa-arrow-right" aria-hidden="true" />{' '}
-                  <i className="fa fa-arrow-right" aria-hidden="true" />
-                  <br /><br />Or you can stay home and enjoy photos from your
-                  last trip. :-)
-                </div>
-              : null}
-
-            <div className="button--huge">
-              <div className="count">
-                <CitiesCount
-                  count={
-                    noCitiesToShow ? '1,634,573' : `${this.state.data.total}`
-                  }
-                />
-              </div>
-              <div className="">
-                places that match your needs
-              </div>
-            </div>
-          </div>
+          <Sidebar
+            total={this.state.data.total}
+            chosenTiles={this.state.chosenTiles}
+            noCitiesToShow={noCitiesToShow}
+          />
 
           <div className="column--right">
             <Search applySearch={this.handleSearch} />
---
+++ src/Sidebar.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import CitiesCount from './CitiesCount';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="column--left">
+        <div className="logo" />
+
+        <div className="title">Where do you wanna go?</div>
+        <div className="location">
+          <div className="place">
+            <i className="fa fa-globe" aria-hidden="true" /> Literally
+            anywhere.
+          </div>
+          <div className="button--link">choose some</div>
+        </div>
+
+        <div className="title">What will you enjoy on your trip?</div>
+
+        {this.props.chosenTiles.map(tile =>
+          <div
+            className={`interest ${tile.nameC}`}
+            key={tile.tagQuery + tile.name}
+          >
+            <div className="name">
+              <div className="interest-icon" />
+              {tile.name}
+            </div>
+            <div className="icon" onClick={() => this.removeTile(tile)}>
+              <i className="fa fa-times" aria-hidden="true" />
+            </div>
+          </div>,
+        )}
+
+        {this.props.chosenTiles.length === 0
+          ? <div className="empty-state">
+              Select some activities.{' '}
+              <i className="fa fa-arrow-right" aria-hidden="true" />{' '}
+              <i className="fa fa-arrow-right" aria-hidden="true" />{' '}
+              <i className="fa fa-arrow-right" aria-hidden="true" />
+              <br /><br />Or you can stay home and enjoy photos from your
+              last trip. :-)
+            </div>
+          : null}
+
+        <div className="button--huge">
+          <div className="count">
+            <CitiesCount
+              count={
+                this.props.noCitiesToShow
+                  ? this.props.chosenTiles.length ? '0' : '1,634,573'
+                  : `${this.props.total}`
+              }
+            />
+          </div>
+          <div className="">
+            places that match your needs
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
